93bbde4d328017761e4148cd9fb3168a7efaa6b5
shubhxm15 <149764503+shubhxm15@users.noreply.github.com>
2024-01-23 18:03:17 +0530

    Add files via upload

---
+++ TakingInput&Output.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+
+    cout<<"Enter your first digit: ";
+    cin>>a;
+
+    cout<<"Enter your seconf digit: ";
+    cin>>b;
+    
+    cout<<"The sum is: "<<a+b;
+        return 0;
+}
