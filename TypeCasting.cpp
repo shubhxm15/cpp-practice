32ca72e49e6ad52ae8f4761afaac5082cc905fd5
Shubham <149764503+shubhxm15@users.noreply.github.com>
2024-01-25 21:49:36 +0530

    Add files via upload

---
+++ TypeCasting.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float x = 123.1234;
+
+    cout<<"Integer value of x is: "<<int(x);
+                 // OR                                 // Basically its a way to change point(.) value into integer type
+    cout<<"\nInteger value of x is: "<<(int)x<<endl;
+
+        return 0;
+}
