e8cc0d1daacca76651a5d5166745b5d4d4d74824
shubhxm15 <149764503+shubhxm15@users.noreply.github.com>
2024-01-25 21:32:30 +0530

    Add files via upload

---
+++ ReferenceVariable.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 69;
+    int & b = a;                // Reference variable 
+                                // Example:- 1 person with different names
+    cout<<"Value of b is: "<<b;
+        return 0;
+}
