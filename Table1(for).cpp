d2b809615f4a59cbb5afcf0bea67d5a928d45b24
Shubham <149764503+shubhxm15@users.noreply.github.com>
2024-01-28 20:48:39 +0530

    Add files via upload

---
+++ Table1(for).cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i=1;
+    int x=7;  
+    for(int i=1; i<=10; ++i)
+    {
+        cout<<x<<" * "<<i<<" = "<<x*i<<endl;
+    }
+    return 0;
+}   
